f91bcf079420184af9df980b300815be569b263e
Joe Polny <joepolny@gmail.com>
2025-01-19 08:16:33 -0500

    replace telescope with snacks picker

--- init.lua
+++ init.lua
@@ -291,6 +291,7 @@ require('lazy').setup({
           -- wo = { wrap = true } -- Wrap notifications
         },
       },
+      picker = {},
     },
     keys = {
       {
@@ -339,6 +340,46 @@ require('lazy').setup({
         function() Snacks.notifier.hide() end,
         desc = 'Dismiss All Notifications',
       },
+
+      --- picker
+      { '<leader>,', function() Snacks.picker.buffers() end, desc = 'Find Buffers' },
+      { '<leader>/', function() Snacks.picker.grep() end, desc = 'Grep Current Buffer' },
+      { '<leader>:', function() Snacks.picker.command_history() end, desc = 'Search Command History' },
+      -- find
+      { '<leader>fb', function() Snacks.picker.buffers() end, desc = '[F]ind [B]uffers' },
+      { '<leader>fc', function() Snacks.picker.files { cwd = vim.fn.stdpath 'config' } end, desc = '[F]ind [C]onfig File' },
+      { '<leader>ff', function() Snacks.picker.files() end, desc = '[F]ind [F]iles' },
+      { '<leader>fg', function() Snacks.picker.git_files() end, desc = '[F]ind [G]it Files' },
+      -- git
+      { '<leader>gc', function() Snacks.picker.git_log() end, desc = 'Search [G]it [C]ommits' },
+      { '<leader>gs', function() Snacks.picker.git_status() end, desc = 'Search [G]it [S]tatus' },
+      -- Grep
+      { '<leader>sb', function() Snacks.picker.lines() end, desc = '[S]earch [B]uffer Lines' },
+      { '<leader>sB', function() Snacks.picker.grep_buffers() end, desc = '[S]earch Open [B]uffers' },
+      { '<leader>sw', function() Snacks.picker.grep_word() end, desc = 'Search Visual Selection or [W]ord', mode = { 'n', 'x' } },
+      -- search
+      { '<leader>s"', function() Snacks.picker.registers() end, desc = '[S]earch Registers' },
+      { '<leader>sa', function() Snacks.picker.autocmds() end, desc = '[S]earch [A]utocmds' },
+      { '<leader>sc', function() Snacks.picker.command_history() end, desc = '[S]earch [C]ommand History' },
+      { '<leader>sC', function() Snacks.picker.commands() end, desc = '[S]earch [C]ommands' },
+      { '<leader>sd', function() Snacks.picker.diagnostics() end, desc = '[S]earch [D]iagnostics' },
+      { '<leader>sh', function() Snacks.picker.help() end, desc = '[S]earch [H]elp Pages' },
+      { '<leader>sH', function() Snacks.picker.highlights() end, desc = '[S]earch [H]ighlights' },
+      { '<leader>sj', function() Snacks.picker.jumps() end, desc = '[S]earch [J]umps' },
+      { '<leader>sk', function() Snacks.picker.keymaps() end, desc = '[S]earch [K]eymaps' },
+      { '<leader>sl', function() Snacks.picker.loclist() end, desc = '[S]earch [L]ocation List' },
+      { '<leader>sM', function() Snacks.picker.man() end, desc = '[S]earch [M]an Pages' },
+      { '<leader>sm', function() Snacks.picker.marks() end, desc = '[S]earch [M]arks' },
+      { '<leader>sR', function() Snacks.picker.resume() end, desc = '[S]earch [R]esume' },
+      { '<leader>sq', function() Snacks.picker.qflist() end, desc = '[S]earch [Q]uickfix List' },
+      { '<leader>sS', function() Snacks.picker.colorschemes() end, desc = '[S]earch Color [S]phemes' },
+      { '<leader>sp', function() Snacks.picker.projects() end, desc = '[S]earch [P]rojects' },
+      { '<leader>ss', function() Snacks.picker.lsp_symbols() end, desc = '[S]earch LSP [S]ymbols' },
+      -- LSP
+      { 'gd', function() Snacks.picker.lsp_definitions() end, desc = '[G]oto [D]efinition' },
+      { 'gr', function() Snacks.picker.lsp_references() end, nowait = true, desc = '[G]oto [R]eferences' },
+      { 'gI', function() Snacks.picker.lsp_implementations() end, desc = '[G]oto Implementation' },
+      { 'gy', function() Snacks.picker.lsp_type_definitions() end, desc = '[G]oto T[y]pe Definition' },
     },
     init = function()
       vim.api.nvim_create_autocmd('User', {
@@ -493,112 +534,6 @@ require('lazy').setup({
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-  { -- Fuzzy Finder (files, lsp, etc)
-    'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for installation instructions
-        'nvim-telescope/telescope-fzf-native.nvim',
-
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
-        build = 'make',
-
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
-        cond = function() return vim.fn.executable 'make' == 1 end,
-      },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
-
-      -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-    },
-    config = function()
-      -- Telescope is a fuzzy finder that comes with a lot of different things that
-      -- it can fuzzy find! It's more than just a "file finder", it can search
-      -- many different aspects of Neovim, your workspace, LSP, and more!
-      --
-      -- The easiest way to use Telescope, is to start by doing something like:
-      --  :Telescope help_tags
-      --
-      -- After running this command, a window will open up and you're able to
-      -- type in the prompt window. You'll see a list of `help_tags` options and
-      -- a corresponding preview of the help.
-      --
-      -- Two important keymaps to use while in Telescope are:
-      --  - Insert mode: <c-/>
-      --  - Normal mode: ?
-      --
-      -- This opens a window that shows you all of the keymaps for the current
-      -- Telescope picker. This is really useful to discover what Telescope can
-      -- do as well as how to actually do it!
-
-      -- [[ Configure Telescope ]]
-      -- See `:help telescope` and `:help telescope.setup()`
-      require('telescope').setup {
-        -- You can put your default mappings / updates / etc. in here
-        --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
-        },
-      }
-
-      -- Enable Telescope extensions if they are installed
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
-
-      -- See `:help telescope.builtin`
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
-
-      -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = '[/] Fuzzily search in current buffer' })
-
-      -- It's also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set(
-        'n',
-        '<leader>s/',
-        function()
-          builtin.live_grep {
-            grep_open_files = true,
-            prompt_title = 'Live Grep in Open Files',
-          }
-        end,
-        { desc = '[S]earch [/] in Open Files' }
-      )
-
-      -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
-    end,
-  },
-
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -671,31 +606,6 @@ require('lazy').setup({
             mode = mode or 'n'
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
-
-          -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-t>.
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
-          -- Find references for the word under your cursor.
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-          -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-          -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
-          map('gy', require('telescope.builtin').lsp_type_definitions, '[G]oto type')
-
-          -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-
-          -- Fuzzy find all the symbols in your current workspace.
-          --  Similar to document symbols, except searches over your entire project.
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
